bba881db2cc3c6c8d60625db55e9d00718c3277d
cottluke1 <cottluke1@gmail.com>
2025-06-11 20:08:49 -0500

    Create source_images.js

---
+++ source_images.js
@@ -0,0 +1,18 @@
+// /js/source_images.js
+
+const sourceImageMap = {
+    "Reuters": "...", // Add your image address for Reuters here
+    "Bloomberg": "...",
+    "CNBC": "...",
+    "Forbes": "...",
+    "BBC News": "...",
+    "Seeking Alpha": "...",
+    "Wall Street Journal": "...",
+    "Financial Times": "...",
+    "Yahoo Finance": "...",
+    "OilPrice.com": "...",
+    "Rigzone": "...",
+    "World Oil": "...",
+    "Hart Energy": "..."
+    // Add more sources and their image addresses as needed
+};
