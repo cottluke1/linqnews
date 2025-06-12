2fa00c0772935df2afa5f6b4cac31a2df9b4d0bc
cottluke1 <cottluke1@gmail.com>
2025-06-12 10:17:08 -0500

    Update articles_premium.js

--- articles_premium.js
+++ articles_premium.js
@@ -1,6 +1,6 @@
 // /js/articles_premium.js
 
-const premiumArticles = [
+window.premiumArticles = [
     {
         id: "prem1",
         category: 'Market Analysis',
@@ -182,6 +182,3 @@ const premiumArticles = [
         premium: true
     }
 ];
-
-window.premiumArticles = premiumArticles;
-export default premiumArticles;
