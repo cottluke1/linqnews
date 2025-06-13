70d0874c879a845d71e69ba9a84ead4dbe03f59e
cottluke1 <cottluke1@gmail.com>
2025-06-12 22:42:19 -0500

    Update articles_present.js

--- articles_present.js
+++ articles_present.js
@@ -3,282 +3,162 @@
 window.presentDayArticles = [
     {
         id: "p1",
-        category: 'Company News',
-        title: "Trump's energy dominance agenda could be ravaged by Section 899",
+        category: 'Power',
+        title: 'Nokia Expands IP Routing Portfolio to Utilities with New Platforms to Boost Smart Grid',
         date: 'June 12, 2025',
-        source: 'Reuters',
-        link: 'https://www.reuters.com/markets/commodities/trumps-energy-dominance-agenda-could-be-ravaged-by-section-899-2025-06-12/'
+        source: 'Seeking Alpha',
+        link: 'https://seekingalpha.com/pr/20135065-nokia-expands-ip-routing-portfolio-to-utilities-with-new-platforms-to-boost-smart-grid'
     },
     {
         id: "p2",
-        category: 'Company News',
-        title: 'Trump to attend AI energy summit in Pittsburgh',
+        category: 'Earnings',
+        title: 'Energy Transfer Has A Strong Yield And Growth Potential',
         date: 'June 12, 2025',
-        source: 'Reuters',
-        link: 'https://www.reuters.com/world/us/trump-attend-ai-energy-summit-pittsburgh-2025-06-12/'
+        source: 'Seeking Alpha',
+        link: 'https://seekingalpha.com/article/4791526-energy-transfer-has-a-strong-yield-and-growth-potential'
     },
     {
         id: "p3",
-        category: 'Natural Gas',
-        title: 'NextDecade signs $9 billion contracts with Bechtel for LNG sites',
+        category: 'Earnings',
+        title: 'Energy Transfer Is My Largest Holding For Good Reason',
+        date: 'June 12, 2025',
+        source: 'Seeking Alpha',
+        link: 'https://seekingalpha.com/article/4794381-energy-transfer-is-my-largest-holding-for-good-reason'
+    },
+    {
+        id: "p4",
+        category: 'Earnings',
+        title: 'Devon Energy: Creating Value With Low Oil Prices',
+        date: 'June 12, 2025',
+        source: 'Seeking Alpha',
+        link: 'https://seekingalpha.com/article/4794278-devon-energy-creating-value-with-low-oil-prices'
+    },
+    {
+        id: "p5",
+        category: 'Earnings',
+        title: 'Duke Energy Keeps Sparking',
+        date: 'June 12, 2025',
+        source: 'Seeking Alpha',
+        link: 'https://seekingalpha.com/article/4794276-duke-energy-keeps-sparking'
+    },
+    {
+        id: "p6",
+        category: 'Company News',
+        title: 'Capital Clean Energy Carriers: A New Name, A New Growth Engine, A Compelling Buy',
+        date: 'June 12, 2025',
+        source: 'Seeking Alpha',
+        link: 'https://seekingalpha.com/article/4794442-capital-clean-energy-carriers-a-new-name-a-new-growth-engine-a-compelling-buy'
+    },
+    {
+        id: "p7",
+        category: 'Earnings',
+        title: 'Clearway Energy: 6% Yield On Utilities, Undervalued, 20 Straight Hikes',
+        date: 'June 12, 2025',
+        source: 'Seeking Alpha',
+        link: 'https://seekingalpha.com/article/4793020-clearway-energy-6-percent-yield-on-utilities-undervalued-20-straight-hikes'
+    },
+    {
+        id: "p8",
+        category: 'Power',
+        title: 'EPA Greenhouse Gas, Mercury Power Plant Rules Finalized',
+        date: 'June 12, 2025',
+        source: 'CT Mirror',
+        link: 'https://ctmirror.org/2025/06/12/epa-greenhouse-gas-mercury-power-plant-rules/'
+    },
+    {
+        id: "p9",
+        category: 'Power',
+        title: 'Victorian Grid Under Pressure As Coal And Gas Outages Hit Supply',
+        date: 'June 12, 2025',
+        source: 'The Australian',
+        link: 'https://www.theaustralian.com.au/business/victorian-grid-under-pressure-as-coal-and-gas-outages-hit-supply/news-story/e38ab0d8c0e7f74853e892eb8f23459e'
+    },
+    {
+        id: "p10",
+        category: 'Power',
+        title: 'Trump EPA Pollution Rules Overturned',
+        date: 'June 12, 2025',
+        source: 'The Guardian',
+        link: 'https://www.theguardian.com/us-news/2025/jun/12/trump-epa-pollution-rules'
+    },
+    {
+        id: "p11",
+        category: 'Crude Oil',
+        title: 'JP Morgan Maintains 2025 Forecast for Oil Prices in Low to Mid-$60s',
+        date: 'June 12, 2025',
+        source: 'Reuters',
+        link: 'https://www.reuters.com/business/energy/jp-morgan-maintains-2025-forecast-oil-prices-low-to-mid-60s-2025-06-12/'
+    },
+    {
+        id: "p12",
+        category: 'Crude Oil',
+        title: 'Saudi Arabia Is Grabbing Oil Market Share, But It Can Open The Tap Only So Far',
+        date: 'June 12, 2025',
+        source: 'Wall Street Journal',
+        link: 'https://www.wsj.com/business/energy-oil/saudi-arabia-is-grabbing-oil-market-share-but-it-can-open-the-tap-only-so-far-6f79fe49'
+    },
+    {
+        id: "p13",
+        category: 'Crude Oil',
+        title: 'This Crucial Corner of the Energy Market Is At High Risk from All-Out War Between Israel and Iran—And It’s Not Oil',
+        date: 'June 12, 2025',
+        source: 'MarketWatch',
+        link: 'https://www.marketwatch.com/story/this-crucial-corner-of-the-energy-market-is-at-high-risk-from-an-all-out-war-between-israel-and-iran-and-its-not-oil-0d26bfbf'
+    },
+    {
+        id: "p14",
+        category: 'Company News',
+        title: 'NextDecade Signs $9 Billion Contracts With Bechtel for LNG Sites',
         date: 'June 12, 2025',
         source: 'Reuters',
         link: 'https://www.reuters.com/business/energy/nextdecade-signs-9-billion-contracts-with-bechtel-construction-lng-sites-2025-06-12/'
     },
     {
-        id: "p4",
-        category: 'Company News',
-        title: 'Britain ready to implement US tariff deal, trade minister says',
-        date: 'June 12, 2025',
-        source: 'Reuters',
-        link: 'https://www.reuters.com/sustainability/climate-energy/britain-ready-implement-us-tariff-deal-trade-minister-says-2025-06-12/'
-    },
-    {
-        id: "p5",
-        category: 'Natural Gas',
-        title: 'Weekly storage of natural gas in U.S. increases: EIA-Xinhua',
-        date: 'June 13, 2025',
-        source: 'Xinhua',
-        link: 'https://english.news.cn/northamerica/20250613/2f735ff374d543268ca71a393ed456a1/c.html'
-    },
-    {
-        id: "p6",
-        category: 'Natural Gas',
-        title: 'United States Natural Gas Stocks Change',
-        date: 'June 12, 2025',
-        source: 'Trading Economics',
-        link: 'https://tradingeconomics.com/united-states/natural-gas-stocks-change/news/462650'
-    },
-    {
-        id: "p7",
-        category: 'Power',
-        title: 'Record US data center power usage amid AI boom, forecasts EIA',
-        date: 'June 12, 2025',
-        source: 'RCR Wireless News',
-        link: 'https://www.rcrwireless.com/20250612/energy/eia-power-ai-data'
-    },
-    {
-        id: "p8",
-        category: 'Company News',
-        title: 'HPCL, BPCL, IOC shares fall up to 4% as Brent crude breaches $70/bbl',
-        date: 'June 12, 2025',
-        source: 'Business Today',
-        link: 'https://www.businesstoday.in/markets/stocks/story/hpcl-bpcl-ioc-shares-fall-brent-crude-price-rises-middle-east-tensions-480099-2025-06-12'
-    },
-    {
-        id: "p9",
-        category: 'Power',
-        title: 'Daily on Energy: California EV rules canceled, global boost for nuclear, Wright on oil',
-        date: 'June 12, 2025',
-        source: 'Washington Examiner',
-        link: 'https://www.washingtonexaminer.com/daily-on-energy/3440160/daily-on-energy-california-ev-rules-canceled-global-boost-nuclear-wright-talks-oil/'
-    },
-    {
-        id: "p10",
-        category: 'Petrochemicals',
-        title: 'China-US trade deal kicks the rare earths can down the road',
-        date: 'June 12, 2025',
-        source: 'Reuters',
-        link: 'https://www.reuters.com/markets/commodities/china-us-trade-deal-kicks-rare-earths-can-down-road-2025-06-12/'
-    },
-    {
-        id: "p11",
-        category: 'Renewables',
-        title: 'Meta signs deal for advanced geothermal power in New Mexico',
-        date: 'June 12, 2025',
-        source: 'Reuters',
-        link: 'https://www.reuters.com/sustainability/boards-policy-regulation/meta-signs-deal-advanced-geothermal-power-new-mexico-2025-06-12/'
-    },
-    {
-        id: "p12",
-        category: 'Power',
-        title: 'Meta-Constellation virtual PPA could be first of many reactor deals: experts',
-        date: 'June 12, 2025',
-        source: 'Utility Dive',
-        link: 'https://www.utilitydive.com/news/meta-constellation-ppa-could-be-first-of-many-deals-for-existing-reactors/750567/'
-    },
-    {
-        id: "p13",
-        category: 'Renewables',
-        title: 'Maryland residential solar loan program takes off despite rough market',
-        date: 'June 12, 2025',
-        source: 'Utility Dive',
-        link: 'https://www.utilitydive.com/news/maryland-residential-solar-loans-green-bank-montgomery-county/750383/'
-    },
-    {
-        id: "p14",
-        category: 'Power',
-        title: 'Demand response program in New York cuts peak energy demand: ISO',
-        date: 'June 12, 2025',
-        source: 'Utility Dive',
-        link: 'https://www.utilitydive.com/news/demand-response-dr-new-york-energy-demand-iso/750400/'
-    },
-    {
         id: "p15",
-        category: 'Power',
-        title: 'Talen, Amazon AWS partner on Susquehanna nuclear data center',
+        category: 'Company News',
+        title: 'TotalEnergies to Collaborate With Mistral AI to Increase Efficiency',
         date: 'June 12, 2025',
-        source: 'Utility Dive',
-        link: 'https://www.utilitydive.com/news/talen-amazon-aws-susquehanna-nuclear-data-centert/750440/'
+        source: 'Yahoo Finance',
+        link: 'https://finance.yahoo.com/news/totalenergies-collaborate-mistral-ai-increase-061500813.html'
     },
     {
         id: "p16",
-        category: 'Renewables',
-        title: 'DOE-House debate on baseload wind & solar reliability',
+        category: 'Company News',
+        title: 'US Energy Loan Office to Fund More Projects in 2025',
         date: 'June 12, 2025',
-        source: 'Utility Dive',
-        link: 'https://www.utilitydive.com/news/doe-wright-house-baseload-wind-solar-reliability/750421/'
+        source: 'Yahoo Finance',
+        link: 'https://finance.yahoo.com/news/us-energy-loan-office-fund-181503597.html'
     },
     {
         id: "p17",
-        category: 'Power',
-        title: 'US utility-scale energy storage to double, reach 65 GW by 2027: EIA',
+        category: 'Natural Gas',
+        title: 'JERA Expects US LNG Share To Reach 30% After Latest Gulf Coast Deals',
         date: 'June 12, 2025',
-        source: 'Utility Dive',
-        link: 'https://www.utilitydive.com/news/us-utility-scale-energy-storage-to-double-reach-65-gw-by-2027-eia/750338/'
+        source: 'S&P Global',
+        link: 'https://www.spglobal.com/commodity-insights/en/news-research/latest-news/lng/061225-jera-expects-us-lng-share-to-reach-30-after-latest-55-mil-mtyear-gulf-coast-deals'
     },
     {
         id: "p18",
-        category: 'Renewables',
-        title: 'Atlantic Shores offshore wind OREC termination in New Jersey',
+        category: 'Natural Gas',
+        title: 'Oil Futures Crude Down On Renewed Tariff Fears',
         date: 'June 12, 2025',
-        source: 'Utility Dive',
-        link: 'https://www.utilitydive.com/news/atlantic-shores-offshore-wind-terminate-new-jersey-orec/750279/'
+        source: 'S&P Global',
+        link: 'https://www.spglobal.com/commodity-insights/en/news-research/latest-news/crude-oil/061225-oil-futures-crude-down-on-renewed-tariff-fears'
     },
     {
         id: "p19",
-        category: 'Company News',
-        title: 'GOP lawmakers ask for clean energy credit tweaks in reconciliation bill',
+        category: 'Policy & Markets',
+        title: 'Trump’s Energy Dominance Agenda Could Be Ravaged By Section 899',
         date: 'June 12, 2025',
-        source: 'Utility Dive',
-        link: 'https://www.utilitydive.com/news/gop-lawmakers-ask-clean-energy-credit-tweaks-reconciliation-one-big-beautiful-bill/750331/'
+        source: 'Reuters',
+        link: 'https://www.reuters.com/markets/commodities/trumps-energy-dominance-agenda-could-be-ravaged-by-section-899-2025-06-12/'
     },
     {
         id: "p20",
         category: 'Company News',
-        title: 'Residential solar installer Sunnova files for bankruptcy, plans sale',
+        title: 'Trump to Attend AI + Energy Summit in Pittsburgh',
         date: 'June 12, 2025',
-        source: 'Utility Dive',
-        link: 'https://www.utilitydive.com/news/residential-solar-installer-sunnova-files-for-bankruptcy-plans-to-sell-and/750387/'
-    },
-    {
-        id: "p21",
-        category: 'Power',
-        title: 'NERC-MISO reliability market monitor highlights FERC capacity issues',
-        date: 'June 12, 2025',
-        source: 'Utility Dive',
-        link: 'https://www.utilitydive.com/news/nerc-miso-reliability-market-monitor-ferc-capacity/750037/'
-    },
-    {
-        id: "p22",
-        category: 'Power',
-        title: 'MISO-FERC fast-track interconnection ERA rules move forward',
-        date: 'June 12, 2025',
-        source: 'Utility Dive',
-        link: 'https://www.utilitydive.com/news/miso-ferc-fast-track-interconnection-eras/750260/'
-    },
-    {
-        id: "p23",
-        category: 'Power',
-        title: 'OMS-MISO resource adequacy survey in Midcontinent region',
-        date: 'June 12, 2025',
-        source: 'Utility Dive',
-        link: 'https://www.utilitydive.com/news/oms-miso-resource-adequacy-survey-midcontinent/750140/'
-    },
-    {
-        id: "p24",
-        category: 'Company News',
-        title: 'Energy-as-a-Service model offers lower costs, predictability: Renew',
-        date: 'June 12, 2025',
-        source: 'Utility Dive',
-        link: 'https://www.utilitydive.com/news/energy-as-a-service-lower-costs-add-predictability-Renew-Charlie-Lord-EaaS-/750551/'
-    },
-    {
-        id: "p25",
-        category: 'Power',
-        title: 'DOE cancels carbon capture decarbonization awards, grants',
-        date: 'June 12, 2025',
-        source: 'Utility Dive',
-        link: 'https://www.utilitydive.com/news/doe-cancel-carbon-capture-decarbonization-awards-grants/749409/'
-    },
-    {
-        id: "p26",
-        category: 'Power',
-        title: 'Electric Supply Chain Act (HR 3638) advances in House',
-        date: 'June 12, 2025',
-        source: 'Utility Dive',
-        link: 'https://www.utilitydive.com/news/Electric-Supply-Chain-Act-Latta-NEMA-HR3638/750137/'
-    },
-    {
-        id: "p27",
-        category: 'Power',
-        title: 'Tariffs to spike power generation costs, report finds',
-        date: 'June 12, 2025',
-        source: 'Utility Dive',
-        link: 'https://www.utilitydive.com/news/tariffs-to-spike-power-generation-costs-reports/750133/'
-    },
-    {
-        id: "p28",
-        category: 'Power',
-        title: 'PJM-FERC data center capacity market updates',
-        date: 'June 12, 2025',
-        source: 'Utility Dive',
-        link: 'https://www.utilitydive.com/news/pjm-ferc-data-center-capacity-market/749916/'
-    },
-    {
-        id: "p29",
-        category: 'Power',
-        title: 'GB Energy budget to fund £2.5 bn nuclear SMR programme',
-        date: 'June 12, 2025',
-        source: 'Energy Voice',
-        link: 'https://www.energyvoice.com/renewables-energy-transition/nuclear/574267/gb-energy-budget-to-fund-2-5bn-nuclear-smr-programme/'
-    },
-    {
-        id: "p30",
-        category: 'Crude Oil',
-        title: 'Badenoch calls for end to windfall tax and ban on new oil licences',
-        date: 'June 12, 2025',
-        source: 'Energy Voice',
-        link: 'https://www.energyvoice.com/oilandgas/north-sea/574301/badenoch-to-call-for-end-to-windfall-tax-and-ban-on-new-oil-licences/'
-    },
-    {
-        id: "p31",
-        category: 'Renewables',
-        title: 'Spending review boosts north-west nuclear, hydrogen and carbon capture',
-        date: 'June 12, 2025',
-        source: 'Energy Voice',
-        link: 'https://www.energyvoice.com/renewables-energy-transition/nuclear/574259/spending-review-boosts-north-west-nuclear-hydrogen-and-carbon-capture/'
-    },
-    {
-        id: "p32",
-        category: 'Renewables',
-        title: 'UK government backs Inverness & Cromarty Firth green freeport business case',
-        date: 'June 12, 2025',
-        source: 'Energy Voice',
-        link: 'https://www.energyvoice.com/renewables-energy-transition/wind/uk-wind/574279/uk-government-backs-inverness-and-cromarty-firth-green-freeport-business-case/'
-    },
-    {
-        id: "p33",
-        category: 'Crude Oil',
-        title: "Securing the deep UK's subsea supply chain can defend our underwater assets",
-        date: 'June 12, 2025',
-        source: 'Energy Voice',
-        link: 'https://www.energyvoice.com/oilandgas/subsea-exploration-production/574284/securing-the-deep-uks-subsea-supply-chain-can-defend-our-underwater-assets/'
-    },
-    {
-        id: "p34",
-        category: 'Crude Oil',
-        title: 'Alternative oil investment locations beyond the North Sea',
-        date: 'June 12, 2025',
-        source: 'Energy Voice',
-        link: 'https://www.energyvoice.com/oilandgas/574265/alternative-oil-investment-locations-beyond-the-north-sea/'
-    },
-    {
-        id: "p35",
-        category: 'Renewables',
-        title: 'Edinburgh Gravitricity funding hydrogen storage project',
-        date: 'June 12, 2025',
-        source: 'Energy Voice',
-        link: 'https://www.energyvoice.com/renewables-energy-transition/hydrogen/574185/edinburgh-gravitricity-funding-hydrogen-storage/'
+        source: 'Reuters',
+        link: 'https://www.reuters.com/world/us/trump-attend-ai-energy-summit-pittsburgh-2025-06-12/'
     }
 ];
